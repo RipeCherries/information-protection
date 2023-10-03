89c66b45cf4d51ea124940c4af3654d03560dc98
Alexey Egoshin <alexeyegoshin0403@mail.ru>
2023-10-03 21:07:44 +0700

    Add new line and call gost sign

--- lab3/main.py
+++ lab3/main.py
@@ -1,5 +1,6 @@
 from rsa_sign import rsa_sign
 from elgamal_sign import elgamal_sign
+from gost_sign import gost_sign
 
 
 def read_file(path):
@@ -11,7 +12,10 @@ def main():
     message = read_file("data/sample_text.txt")
 
     rsa_sign(message)
+    print()
     elgamal_sign(message)
+    print()
+    gost_sign(message)
 
 
 if __name__ == "__main__":
